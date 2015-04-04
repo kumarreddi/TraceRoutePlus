d560132217698f4ce2366c84bffb30893e2b60a2
Starbeamrainbowlabs <starbeamrainbowlabs@gmail.com>
2015-04-04 18:01:08 +0100

    Fixed help display bug

--- TraceRoutePlus/Program.cs
+++ TraceRoutePlus/Program.cs
@@ -66,7 +66,7 @@ namespace TraceRoutePlus
 				return;
 			}
 
-			if(Options["help"] == "true" || ((args.Length == 0 || Targets.Count == 0) && Options["file"].Length < 0))
+			if(Options["help"] == "true" || ((args.Length == 0 || Targets.Count == 0) && Options["file"].Length <= 0))
 			{
 				Console.WriteLine(Resources.Help);
 				return;
